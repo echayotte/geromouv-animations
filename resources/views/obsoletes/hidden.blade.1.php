4a9f7da2c078f9a1805a8c2bd1aebf98a3093572
echayotte <echayotte@gmail.com>
2018-08-02 21:42:24 +0200

    obsoletes files

---
+++ resources/views/obsoletes/hidden.blade.1.php
@@ -0,0 +1,154 @@
+<section id="{{$section}}" visible="hidden">
+    <div class="entry-content">
+
+        <header class="entry-header">
+            <h2>
+                //──── title ─────────────────────────────────────────────────────────
+                <span>{{$title}}</span>
+            </h2>
+        </header>
+        
+        {{-- @include('/storage/textes/pages/presentation/stages.') --}}
+        //──── video ─────────────────────────────────────────────────────────
+        <iframe src="https://www.youtube.com/embed/{{$video}}?controls=1"></iframe>
+
+        //──── img ───────────────────────────────────────────────────────────
+        <img class="alignleft" src="/storage/images/pages/presentation/{{$img}}-vignette.png">
+
+        //──── text ──────────────────────────────────────────────────────────
+        <p>{{$text}}</p>
+    </div>
+</section>
+
+<hr>
+
+<section>
+    <div class="presentation-infos">
+
+        <div class="entry-content">
+            <header class="entry-header">
+                <h2>
+                    <span>Listes des [Stages] à venir</span>
+                </h2>
+            </header>
+
+            <ul class="presentation-infos__list">
+
+                <li class="presentation-infos__items items">
+                    <ul class="items__sub-items sub-items">
+                        <li class="sub-items__more">
+                            <a href="#" role="button">voir ce stage</a>
+                        </li>
+                        <li class="sub-items__title">[Auprès de la pierre de Rosette]</li>
+                        <li class="sub-items__dot"> :&nbsp;&nbsp;du </li>
+                        <li class="sub-items__date">[20/09]</li>
+                        <li class="sub-items__dot"> au </li>
+                        <li class="sub-items__date">[25/09]</li>
+                        <li class="sub-items__dot"> à </li>
+                        <li class="sub-items__place">[Figeac]</li>
+                        <li class="sub-items__dot">:</li>
+                    </ul>
+                </li>
+
+                <li class="presentation-infos__items items">
+                    <ul class="items__sub-items sub-items">
+                        <li class="sub-items__more">
+                            <a href="#" role="button">voir ce stage</a>
+                        </li>
+                        <li class="sub-items__title">stage xxx</li>
+                        <li class="sub-items__dot">: du </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot"> au </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot">, à</li>
+                        <li class="sub-items__place">[Cahors]</li>
+                        <li class="sub-items__dot">:</li>
+                    </ul>
+                </li>
+
+                <li class="presentation-infos__items items">
+                    <ul class="items__sub-items sub-items">
+                        <li class="sub-items__more">
+                            <a href="#" role="button">voir ce stage</a>
+                        </li>
+                        <li class="sub-items__title">stage xxx</li>
+                        <li class="sub-items__dot">: du </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot"> au </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot">, à</li>
+                        <li class="sub-items__place">[Cahors]</li>
+                        <li class="sub-items__dot">:</li>
+                    </ul>
+                </li>
+
+                <li class="presentation-infos__items items">
+                    <ul class="items__sub-items sub-items">
+                        <li class="sub-items__more">
+                            <a href="#" role="button">voir ce stage</a>
+                        </li>
+                        <li class="sub-items__title">stage xxx</li>
+                        <li class="sub-items__dot">: du </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot"> au </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot">, à</li>
+                        <li class="sub-items__place">[Cahors]</li>
+                        <li class="sub-items__dot">:</li>
+                    </ul>
+                </li>
+
+                <li class="presentation-infos__items items">
+                    <ul class="items__sub-items sub-items">
+                        <li class="sub-items__more">
+                            <a href="#" role="button">voir ce stage</a>
+                        </li>
+                        <li class="sub-items__title">stage xxx</li>
+                        <li class="sub-items__dot">: du </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot"> au </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot">, à</li>
+                        <li class="sub-items__place">[Cahors]</li>
+                        <li class="sub-items__dot">:</li>
+                    </ul>
+                </li>
+
+                <li class="presentation-infos__items items">
+                    <ul class="items__sub-items sub-items">
+                        <li class="sub-items__more">
+                            <a href="#" role="button">voir ce stage</a>
+                        </li>
+                        <li class="sub-items__title">stage xxx</li>
+                        <li class="sub-items__dot">: du </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot"> au </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot">, à</li>
+                        <li class="sub-items__place">[Cahors]</li>
+                        <li class="sub-items__dot">:</li>
+                    </ul>
+                </li>
+
+                <li class="presentation-infos__items items">
+                    <ul class="items__sub-items sub-items">
+                        <li class="sub-items__more">
+                            <a href="#" role="button">voir ce stage</a>
+                        </li>
+                        <li class="sub-items__title">stage xxx</li>
+                        <li class="sub-items__dot">: du </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot"> au </li>
+                        <li class="sub-items__date">[jj mm]</li>
+                        <li class="sub-items__dot">, à</li>
+                        <li class="sub-items__place">[Cahors]</li>
+                        <li class="sub-items__dot">:</li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+        <!-- .entry-content -->
+    </div>
+    <!-- .presentation-infos -->
+</section>
