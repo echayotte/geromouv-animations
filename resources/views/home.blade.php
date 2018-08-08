7a8ef991735c62a1ddacfda023dd26f22bee9581
echayotte <echayotte@gmail.com>
2018-08-08 11:08:10 +0200

    add homepage for developpement

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="/presentation">presentation</a></li>
+    <li><a href="/member/create">formulaire</a></li>
+    <li><a href="">item</a></li>
+    <li><a href="">item</a></li>
+    <li><a href="">item</a></li>
+</ul>
