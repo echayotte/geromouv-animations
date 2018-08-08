69043e92300785097c2e7c86b43cf6c662f4c584
echayotte <echayotte@gmail.com>
2018-08-08 11:11:59 +0200

    simple update

--- resources/views/pages/member/show.blade.php
+++ resources/views/pages/member/show.blade.php
@@ -4,17 +4,16 @@
     liste des inscrits
 @endsection
 
-{{-- section to show the data completed in the DB --}}
 
 
 @section('content')
 
+    {{-- status to show the data completed in the DB --}}
     @if (session('status'))
         <div class="alert alert-success">
             {{ session('status') }}
         </div>
     @endif
-
     <p>
         Vérifiez les informations avant d'imprimer.
         Si tout est bon vous pouvez imprimer, sinon vous pouvez modifier ici.
@@ -22,16 +21,18 @@
     <button>Modifier</button>
     <ul>
         <li><h5>Informations</h5></li>
-        <li>Nom: {{ $member->mem_last_name }} Prénom: {{ $member->mem_first_name }}</li>
-        <li>Date de naissance: {{ $member->mem_date_of_birth }}, Sexe: {{ $member->mem_gender }}</li>
+        <li>Nom: {{ $member->lastname }}</li>
+        <li>Prénom: {{ $member->firstname }}</li>
+        <li>Date de naissance: {{ $formatDate }}</li>
+        <li>Sexe: {{ $member->gender }}</li>
         <li><h5>Contact</h5></li>
-        <li>Adresse: {{ $member->mem_address }} - {{ $member->mem_zipcode }} {{ $member->mem_city }}</li>
-        <li>Email: {{ $member->mem_email }}</li>
-        <li>N° de téléphone fixe: {{ $member->mem_phone }}</li>
-        <li>N° de téléphone portable: {{ $member->mem_cellphone }}</li>
+        <li>Adresse: {{ $member->address }} - {{ $member->zipcode }} {{ $member->city }}</li>
+        <li>Email: {{ $member->email }}</li>
+        <li>N° de téléphone fixe: {{ $member->phone }}</li>
+        <li>N° de téléphone portable: {{ $member->cellphone }}</li>
         <li><h5>Informations de santé</h5>
-        <li>Complémentaire Santé: {{ $member->complementary_health->com_name }}</li>
-        <li>Caisse de Retraite: {{ $member->pension_fund->pen_name }}</li>
+        <li>Complémentaire Santé: {{ $member->mutual->name }}</li>
+        <li>Caisse de Retraite: {{ $member->pension->name }}</li>
         {{-- <li>Activités choisies: {{ $member-> }}</li> --}}
     </ul>
 @endsection
