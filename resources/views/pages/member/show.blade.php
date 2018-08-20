1fa207eb7620dea1826519b174eba61e31b9df08
echayotte <echayotte@gmail.com>
2018-08-20 16:16:20 +0200

    add print button+ activities

--- resources/views/pages/member/show.blade.php
+++ resources/views/pages/member/show.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.layout')
 
 @section('title')
-    liste des inscrits
+    Demande d'inscription de: {{ $member->firstname }} {{ $member->lastname }}aux activités de Géromouv'
 @endsection
-
-
+ 
+@section('css')
+    <link rel='stylesheet' href='/css/print.css' type='text/css' media='print' />
+@endsection
 
 @section('content')
 
@@ -18,7 +20,8 @@
         Vérifiez les informations avant d'imprimer.
         Si tout est bon vous pouvez imprimer, sinon vous pouvez modifier ici.
     </p>
-    <button>Modifier</button>
+    <button>Imprimer</button>
+    <a class="link" href="{{ route('member.edit', $member->id) }}">Modifier</a>
     <ul>
         <li><h5>Informations</h5></li>
         <li>Nom: {{ $member->lastname }}</li>
@@ -28,11 +31,19 @@
         <li><h5>Contact</h5></li>
         <li>Adresse: {{ $member->address }} - {{ $member->zipcode }} {{ $member->city }}</li>
         <li>Email: {{ $member->email }}</li>
-        <li>N° de téléphone fixe: {{ $member->phone }}</li>
-        <li>N° de téléphone portable: {{ $member->cellphone }}</li>
+        <li>N° de téléphone principal: {{ $member->primaryphone }}</li>
+        <li>N° de téléphone secondaire: {{ $member->secondaryphone }}</li>
         <li><h5>Informations de santé</h5>
         <li>Complémentaire Santé: {{ $member->mutual->name }}</li>
         <li>Caisse de Retraite: {{ $member->pension->name }}</li>
-        {{-- <li>Activités choisies: {{ $member-> }}</li> --}}
+        <li>Activités choisies:
+            <ul>
+                @foreach ($member->activities as $activity)
+                    <li> {{ $activity->name}}</li>
+                @endforeach
+            </ul>
+        </li>
     </ul>
+    
+
 @endsection
