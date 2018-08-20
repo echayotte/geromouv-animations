17614181a5187ee6acd38b7d2c36de82bedd267f
echayotte <echayotte@gmail.com>
2018-08-20 16:16:47 +0200

    adding comments

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,12 @@
 Route::view('/', 'home');
 
 Route::get('/presentation', 'PresentationController@show');
+
+// CRUD with Laravel's nomenclature. It makes the routes by himself, don't need to generate them
 Route::resource('/member', 'MemberController');
+
+//You may register many resource controllers at once by passing an array to the resources method:
+// Route::resources([
+//     'photos' => 'PhotoController',
+//     'posts' => 'PostController'
+// ]);
