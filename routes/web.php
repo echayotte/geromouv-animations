d96c38f03803b761451d12674e8f9ef62d32a1cb
echayotte <echayotte@gmail.com>
2018-08-29 10:10:13 +0200

    update routes and cleaning

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,31 @@
 <?php
 
-//──── Authentification ──────────────────────────────────────────────
+//──── AUTHENTICATION ──────────────────────────────────────────────
 Auth::routes();
+// login, register
 Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/', 'pages.presentation.show'); // necessary when logout
 
-Route::view('/', 'pages.presentation.show');
 
-// Route::view('/test/bem', 'pages.test.bem');
-// Route::view('/welcome', 'obsoletes.welcome');
 
 Route::get('/presentation', 'PresentationController@show');
 
-// CRUD with Laravel's nomenclature. It makes the routes by himself, don't need to generate them
-Route::resource('/member', 'MemberController');
-Route::resource('/animation', 'AnimationController');
+// CRUD with Laravel's nomenclature. With resource it makes the routes by himself, don't need to generate them
+// You may register many resource controllers at once by passing an array to the resource(s) method:
+Route::resources([
+    'member' => 'MemberController',
+    'planning_animation' =>'PlanningAnimationController',
+]);
+    
+// see if I change @calendar in @planning
+// Route::get('/planning', ['uses' => 'SampleController@planning']);
+// Route::get('/planning', ['uses' => 'SampleController@calendar']);
 
 
-//You may register many resource controllers at once by passing an array to the resources method:
-// Route::resources([
-//     'photos' => 'PhotoController',
-//     'posts' => 'PostController'
-// ]);
 
 
-Route::resource('/file', 'FileController');
 
+
+
+// Route::view('/test/bem', 'pages.test.bem');
+// Route::view('/welcome', 'obsoletes.welcome');
