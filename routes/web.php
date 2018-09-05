9ad07ba9eef9760910121843efb65c5988c18d7b
echayotte <echayotte@gmail.com>
2018-09-05 15:57:47 +0200

    add tha ajax routing

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::view('/', 'pages.presentation.show'); // necessary when logout
 
 
-Route::get('/presentation', 'PresentationController@show');
+Route::get('presentation', 'PresentationController@show');
 
 // CRUD with Laravel's nomenclature. With resource it makes the routes by himself, don't need to generate them
 // You may register many resource controllers at once by passing an array to the resource(s) method:
 Route::resource('member', 'MemberController');
 
 
+Route::post('ajax', 'AjaxController@ajax_call');
 
 
 // Route::view('/test/bem', 'pages.test.bem');
