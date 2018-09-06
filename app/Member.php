0bbe80fa4627d747a42b8830671a2a9b92495c29
echayotte <echayotte@gmail.com>
2018-09-06 11:46:15 +0200

    add use Sortable by composer         "kyslik/column-sortable": "^5.7"

--- app/Member.php
+++ app/Member.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
 
 class Member extends Model
 {
+    use Sortable;
+
     // needed to format the date first but then it didn't permit to update a member so commented
     // protected $dateFormat = 'Y-m-d';
     protected $dates = ['birthday'];
@@ -13,18 +17,11 @@ class Member extends Model
     // with Controller's function Create:: => Authorize the column to be fillable 
     protected $fillable = [
         //DB's column   
-        'lastname',
-        'firstname',
-        'birthday',
-        'gender',
-        'address',
-        'zipcode',
-        'city',
-        'email',
-        'primaryphone',
-        'mutual_id',
-        'pension_id',
+        'lastname', 'firstname', 'birthday', 'gender', 'address', 'zipcode', 'city', 'email', 'primaryphone', 'mutual_id', 'pension_id',
     ];
+
+    // to sort member's table in member's index.blade.php
+    public $sortable = ['id', 'lastname', 'firstname', 'email'];
 
     // naming theses functions by their foreignkey without the id for Laravel's nomenclature
     // relation one to many (inverse)
