f69885ceba318ebb32445bce23674b484a4cfd53
echayotte <echayotte@gmail.com>
2018-09-11 17:26:29 +0200

    cleaning files

--- resources/views/pages/member/index.blade.php
+++ resources/views/pages/member/index.blade.php
@@ -47,38 +47,6 @@ liste des inscrits
 
     </table>
 
-    {{-- {!! $members->appends(\Request::except('page'))->render() !!} --}}
-
-
-{{-- 
-<table>
-    <tr>
-        <th>Id</th>
-        <th class="nom" style="cursor:pointer">Nom</th>
-        <th>Prénom</th>
-        <th>email</th>
-        <th class="th-right">actions</th>
-        
-    </tr>
-    @foreach ($members as $member)
-    <tr name="row" content="{{$member->id}}">
-        <td class="id" content="{{$member->id}}">{{$member->id}}</td>
-        <td class="lastname" content="{{$member->lastname}}">{{$member->lastname}}</td>
-        <td class="firstname" content="{{$member->firstname}}">{{$member->firstname}}</td>
-        <td class="email" content="{{$member->email}}">{{$member->email}}</td>
-        <td class="table-right"><a class="btn btn--green btn--small" href="{{ route('member.show', $member->id) }}">Voir</a>
-            <a class="btn btn--orange btn--small" href="{{ route('member.edit', $member->id) }}">Modifier</a>
-            
-            <form action="{{ action('MemberController@destroy', $member->id) }}" method="post" style="display: inline;" onsubmit="if(confirm('Vous êtes sur le point de supprimer \n {{ $member->lastname }} {{ $member->firstname }}.\n Êtes-vous sûr?')) { return true } else {return false };">
-                @csrf
-                @method("DELETE")
-                <input class="btn btn--red btn--small" type="submit" id="submit" value="Supprimer" name="submit" style="display: inline;">
-            </form>
-        </td>
-    </tr>
-    @endforeach
-</table> --}}
-
 {{ $members->links() }}
 {{-- @else
 <div class="main-content">
@@ -89,10 +57,4 @@ liste des inscrits
     </section>
 </div>
 @endif --}}
-
-
-@endsection
-
-@section('js')
-    <script src="{{ asset('js/ajax.js') }}"></script>
 @endsection
