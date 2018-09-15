95b1eda8a882920f0194b5fef35961ea1993c362
echayotte <echayotte@gmail.com>
2018-09-15 15:40:01 +0200

    change pagination for working with module, and cleaning

--- resources/views/pages/member/index.blade.php
+++ resources/views/pages/member/index.blade.php
@@ -10,44 +10,50 @@ liste des inscrits
 
 @section('content')
 @if (Auth::check())
+    <div class="entry-content">
+        <h2>Liste des inscrits à Géromouv'</h2>
+        {{-- use @sortable from [Kyslik\ColumnSortable\ColumnSortableServiceProvider] --}}
+        <table>
+            <tr>
+                <th>@sortablelink('id', 'Id')</th>
+                <th>@sortablelink('lastname', 'Nom')</th>
+                <th>@sortablelink('firstname','Prénom')</th>
+                <th>@sortablelink('email', 'Mail')</th>
+                <th class="th-right"></th>
+            </tr>
 
-    <table>
-        <tr>
-            <th>@sortablelink('id', 'Id')</th>
-            <th>@sortablelink('lastname', 'Nom')</th>
-            <th>@sortablelink('firstname','Prénom')</th>
-            <th>@sortablelink('email', 'Mail')</th>
-            <th class="th-right"></th>
-        </tr>
+            @if($members->count())
 
-        @if($members->count())
+                @foreach($members as $key => $member)
 
-            @foreach($members as $key => $member)
+                    <tr>
+                        <td>{{ $member->id }}</td>
+                        <td>{{ $member->lastname }}</td>
+                        <td>{{ $member->firstname }}</td>
+                        <td>{{ $member->email }}</td>
+                        <td class="table-right">
+                            <a class="btn btn--green btn--small" href="{{ route('member.show', $member->id) }}">Voir</a>
+                            <a class="btn btn--orange btn--small" href="{{ route('member.edit', $member->id) }}">Modifier</a>
+                            
+                            <form action="{{ action('MemberController@destroy', $member->id) }}" method="post" style="display: inline;" onsubmit="if(confirm('Vous êtes sur le point de supprimer \n {{ $member->lastname }} {{ $member->firstname }}.\n Êtes-vous sûr?')) { return true } else {return false };">
+                                @csrf
+                                @method("DELETE")
+                                <input class="btn btn--red btn--small" type="submit" id="submit" value="Supprimer" name="submit" style="display: inline;">
+                            </form>
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td>{{ $member->id }}</td>
-                    <td>{{ $member->lastname }}</td>
-                    <td>{{ $member->firstname }}</td>
-                    <td>{{ $member->email }}</td>
-                    <td class="table-right">
-                        <a class="btn btn--green btn--small" href="{{ route('member.show', $member->id) }}">Voir</a>
-                        <a class="btn btn--orange btn--small" href="{{ route('member.edit', $member->id) }}">Modifier</a>
-                        
-                        <form action="{{ action('MemberController@destroy', $member->id) }}" method="post" style="display: inline;" onsubmit="if(confirm('Vous êtes sur le point de supprimer \n {{ $member->lastname }} {{ $member->firstname }}.\n Êtes-vous sûr?')) { return true } else {return false };">
-                            @csrf
-                            @method("DELETE")
-                            <input class="btn btn--red btn--small" type="submit" id="submit" value="Supprimer" name="submit" style="display: inline;">
-                        </form>
-                    </td>
-                </tr>
+                @endforeach
 
-            @endforeach
+            @endif
 
-        @endif
+        </table>
 
-    </table>
-
-{{ $members->links() }}
+        <div class="parent-pagination">
+            {{-- paginate with [Kyslik\ColumnSortable\ColumnSortableServiceProvider] --}}
+            {!! $members->appends(\Request::except('page'))->render() !!}
+        </div>
+    </div>
 @else
 <div class="main-content">
     <section>
