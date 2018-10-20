aa0145156e43be798783661bf8cdff9ce0120fbe
echayotte <echayotte@gmail.com>
2018-10-20 19:03:09 +0200

    add meta for ie edge

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	{{-- mandatory for Laravel if not, $errors --}}
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>
