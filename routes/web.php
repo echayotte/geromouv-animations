c17101882301d0a4e483ec7b361891df24e4b469
echayotte <echayotte@gmail.com>
2018-10-21 18:59:45 +0200

    cleaning

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ Route::get('/admin', 'AdminController@index')->name('admin');
 Route::view('/', 'pages.presentation.show'); // necessary when logout
 
 //──── PAGES ─────────────────────────────────────────────────────────
-
 Route::get('presentation', 'PresentationController@show');
+Route::post('addMutual', 'MutualController@store');
+Route::post('addPension', 'PensionController@store');
 
 /**
  * CRUD with Laravel's nomenclature.
@@ -19,7 +20,4 @@ Route::resources([
     'member' => 'MemberController',
     'mutual' => 'MutualController',
     'pension' => 'PensionController',
-    ]);
-
-Route::post('/addMutual', 'MutualController@store');
-Route::post('/addPension', 'PensionController@store');
+]);
