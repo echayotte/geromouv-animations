ddca080f87ec5d46786b635aec8be45d47e8c27c
echayotte <echayotte@gmail.com>
2018-10-21 14:47:21 +0200

    even better with if-elsif-else

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,20 +7,17 @@
 
 <div class="top-right links">
     @guest
-    @if(Request::is('login'))
-    <a href="{{ route('register') }}">Inscription</a>
-    @else
-    @endif 
-    @if(Request::is('register'))
-    <a href="{{ route('login') }}">Connexion</a>
-    @else
-    @endif
-    
-    @else
-    <a href="{{ route('logout') }}"
-        onclick="event.preventDefault();document.getElementById('logout-form').submit();">Déconnexion</a>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-    </form>
+        @if(Request::is('login'))
+            <a href="{{ route('register') }}">Inscription</a>
+        @elseif(Request::is('register'))
+            <a href="{{ route('login') }}">Connexion</a>
+        @endif
+
+        @else
+            <a href="{{ route('logout') }}"
+                onclick="event.preventDefault();document.getElementById('logout-form').submit();">Déconnexion</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
     @endguest
 </div>
