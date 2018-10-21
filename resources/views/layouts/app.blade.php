5aa4d8374caf971918da1df37a3bf0837f5cdbb7
echayotte <echayotte@gmail.com>
2018-10-21 14:51:24 +0200

    delete toggle navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,4 @@
 
-{{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-    <span class="navbar-toggler-icon"></span>
-</button> --}}
-
-
-
 <div class="top-right links">
     @guest
         @if(Request::is('login'))
